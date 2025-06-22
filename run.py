47296fcedff14d5b9caee3c33a1ba9ede3057ec7
Cynthia Pinedo <cynthiapinedoh79@gmail.com>
2025-06-22 23:43:59 +0000

    install gspreadgspread oauth2client, requirements.txt and adding text to gitignore

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from oauth2client.service_account import Credentials
+
+def get_google_sheet(sheet_name, worksheet_name):
+    SCOPE = ["https://spreadsheets.google.com/feeds", 
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+    # Load credentials from JSON keyfile
+    CREDS = Credentials.from_service_account_file("creds.json")
+    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+    SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+
+    worksheet = SHEET.worksheet(worksheet_name)
+    return worksheet
+
